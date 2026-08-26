05e3e63d9148de86b8f4c50fef713a87fd9e143d
Elucian Moise <elucian.moise@gmail.com>
2026-08-25 21:32:47 -0400

    Map core AST nodes: Declaration, IfStatement, BinaryExpression

--- internal/parser/ast_nodes.go
+++ internal/parser/ast_nodes.go
@@ -1,31 +1,70 @@
-// internal/ast/nodes.go
+// internal/parser/ast_nodes.go
 package ast
 
 import "bee/token"
 
+// Node represents the base interface for all AST elements
 type Node interface {
     Pos() token.Pos
 }
 
+// Statement defines a node that represents a Bee statement
 type Statement interface {
     Node
     statementNode()
 }
 
+// Expression defines a node that represents a Bee expression
 type Expression interface {
     Node
     expressionNode()
 }
 
+// ----------------------------------------------------------------------------
 // Declarations
+// ----------------------------------------------------------------------------
+
 type Declaration struct {
-    Token token.Token
+    Token token.Token // "new" or "set"
     Name  *Identifier
+    Type  Expression
     Value Expression
+}
+func (d *Declaration) statementNode() {}
+func (d *Declaration) Pos() token.Pos { return d.Token.Pos }
+
+type Identifier struct {
+    Token token.Token
+    Value string
+}
+func (i *Identifier) expressionNode() {}
+func (i *Identifier) Pos() token.Pos { return i.Token.Pos }
+
+// ----------------------------------------------------------------------------
+// Control Flow
+// ----------------------------------------------------------------------------
+
+type IfStatement struct {
+    Token     token.Token // "if"
+    Condition Expression
+    Body      *Block
+    Else      *Block
+}
+func (i *IfStatement) statementNode() {}
+func (i *IfStatement) Pos() token.Pos { return i.Token.Pos }
+
+type Block struct {
+    Statements []Statement
 }
 
-type Assignment struct {
-    Token token.Token
-    Name  *Identifier
-    Value Expression
+// ----------------------------------------------------------------------------
+// Expressions
+// ----------------------------------------------------------------------------
+
+type BinaryExpression struct {
+    Token    token.Token // operator
+    Left     Expression
+    Right    Expression
 }
+func (b *BinaryExpression) expressionNode() {}
+func (b *BinaryExpression) Pos() token.Pos { return b.Token.Pos }
